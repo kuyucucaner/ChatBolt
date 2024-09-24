a32e1378e2b74fcdfe1883d7ff796e44efe5f6ef
kuyucucaner <tahacanerkuyucu@gmail.com>
2024-09-24 23:25:28 +0300

    frontend

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./config/database'); 
+require('dotenv').config();
+
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Server Error!');
+});
+
+// Port Ayarlama ve Sunucuyu Başlatma
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
